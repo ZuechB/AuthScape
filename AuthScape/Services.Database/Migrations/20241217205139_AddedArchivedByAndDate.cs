722f2fbcc71fbd91b2cd12a2ed3f460117ea6e87
Brandon Zuech <Brandonzuech@hotmail.com>
2024-12-17 20:31:04 -0500

    updating

---
+++ AuthScape/Services.Database/Migrations/20241217205139_AddedArchivedByAndDate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Services.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedArchivedByAndDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ArchivedBy",
+                table: "DocumentComponents",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArchivedDate",
+                table: "DocumentComponents",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchivedBy",
+                table: "DocumentComponents");
+
+            migrationBuilder.DropColumn(
+                name: "ArchivedDate",
+                table: "DocumentComponents");
+        }
+    }
+}
